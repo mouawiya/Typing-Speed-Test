4a721893e8c026ff293413d5fc7a39a377c0a4e9
mouawiya <mouawiya93@gmail.com>
2023-02-05 08:01:22 +0000

    Fill the list of test words with shuffle array, random capitalisation and punctuation

--- dataModule.js
+++ dataModule.js
@@ -1,5 +1,86 @@
 var dataModule = (function (){
 
+    var lineReturn = '|';
+
+    // shuffle function: shuffle an array
+        var shuffle=function(array){
+            // [1, 2, 3] -> [2, 3, 1]
+            // newArray[]
+            // select rendom element: 2
+            // newArray[2]
+            // oldArray[1, 3]
+            // select random element: 1
+            // newArray[2, 1]
+            // oldArray[3]
+            // select random element: 3
+            // newArray[2, 1, 3]
+            // oldArray[]
+
+            var newArray=[];
+            var randomIndex;
+            var randomElement;
+
+            // a while loop while the array is not empty
+            while(array.length>0){
+                // take a random element from array and add it to newArray
+                randomIndex= Math.floor(Math.random()*array.length); // generate a random index from 0 to the length of the array
+                randomElement = array[randomIndex]; // selecting the random element
+                newArray.push(randomElement); // add the random number at the end of the newArray
+
+                // delete randomElement from array
+                array.splice(randomIndex,1);
+            }
+
+            return newArray;
+        };
+
+    // capitalize first letter of a string
+        String.prototype.capitalize = function(){
+            var newString='';
+            // capitalize the first letter of a string
+            var firstCharCap = this.charAt(0).toUpperCase(); // "this" is used in order to refere to the elements inside the array since these are private methods
+            // get the remaining of the string
+            var remainingChar = this.slice(1)
+            // concat the first and the rest of string
+            newString = firstCharCap+remainingChar;
+            
+
+            return newString;
+        };
+  
+    // capitalizeRandom function: capitalize random words in the array using the above method capitalize
+    // array['word1', 'word2', 'word3']
+    // array['Word1', 'word2', 'Word3']
+        var capitalizeRandom=function(arrayOfStrings){
+
+            // map: apply the method to all elements of the array
+            return arrayOfStrings.map(function(currentWord){
+                var x = Math.floor(4*Math.random()); // will return a number between 0 and 3
+
+                // chances of x equal to 3: 25%
+
+                return (x == 3)? currentWord.capitalize(): currentWord; // we can use this expression or the If(); it is the same
+            });
+        };
+
+    // addRandomPunctuation function: add random punctuation to some words of the array
+    // array['word1', 'word2', 'word3']
+    // array['Word1.', 'word2?', 'Word3,']
+        var addRandomPunctuation=function(arrayOfStrings){
+
+            return arrayOfStrings.map(function(currentWord){
+                var randomPunctuation;
+                var items = [lineReturn,'?', ',', ',', ',', ',', '.', '.', '!', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
+                /* the variable line return is defined at the start with '|' and referes to line returns,
+                and the empty '' is for adding nothing to the words 
+                (because some words won't get any punctuation)
+                some elements are repeated more than others because they are more likely to accure*/
+                var randomIndex= Math.floor(Math.random()*items.length);
+                randomPunctuation = items [randomIndex];
+                return currentWord + randomPunctuation;
+            });
+        };
+
     var appData = {
         indicators: {
             testStarted: false, testEnded: false, totalTestTime:0, timeLeft:0
@@ -53,7 +134,27 @@ var dataModule = (function (){
 
     // test words
         
-        fillListOfTestWords: function(textNumber){}, // fills words.testWords
+        // fills words.testWords
+        fillListOfTestWords: function(textNumber, words){
+
+            // convert the string in an array using the space as a seperator
+            var result = words.split(" ");
+
+            // if we pick the first list of words, some extra treatment is obliged
+            if(textNumber == 0){
+                // shuffle words
+                result = shuffle(result);
+
+                // capitalise random strings
+                result = capitalizeRandom(result);
+
+                // add a random punctuation
+                result = addRandomPunctuation(result);
+            }
+            
+            // fill the testWords with the result 
+            appData.words.testWords = result;
+        },
 
         getListofTestWords: function(){}, // get list of test words: words.testWords
 
@@ -62,5 +163,10 @@ var dataModule = (function (){
         numOfCorrectCharacters and numOfTestCharacters */
 
         updateCurrentWord: function(value) {}, // updates current word using user input
+
+        // just a testing method
+        returnData(){
+            console.log(appData);
+        }
     }
 })();
--- eventsModule.js
+++ eventsModule.js
@@ -9,7 +9,32 @@ var eventsModule = (function(dModule, uModule, cModule, wModule){
     return {
         // init function, initializes the test before start
         init: function(duration, textNumber){
+
+            // fill the list of test words: dataModule
+
+                // get the words from the wordsModule using the getWords method and the textNumber to select the list of words out of 3
+                var words = wModule.getWords(textNumber);
+
+                dModule.fillListOfTestWords(textNumber, words);
+
+            // fill the list of test words: UIModule
+
+            // set the total test time
+
+            // update time left: dataModule
+            
+            // update time left: UIModule
+
+            // move to a new word: dataModule
+
+            // set active word: UIModule
+
+            // format the active word: UIModule
+
+            // focus on text input: UIModule
+
+            // add event listeners 
             addEventListeners();
         }
     };
-})(dataModule, UIModule, cetificateModule, wordsModule);
+})(dataModule, UIModule, certificateModule, wordsModule);
